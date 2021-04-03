480e87baa4454b8e875d12888967ddebd9511c47
asbelljc <asbelljc@gmail.com>
2021-04-03 17:29:55 -0400

    added working darken function; works but seems slow on larger grids - might need to refactor without 'filter: brightness'

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let sizeSlider = document.getElementById("size-slider");
 sizeSlider.value=16; // makes starting grid 16 x 16
 
 function colorIt(e) {
-  e.target.style.backgroundColor = "black";
+  e.target.style.backgroundColor = "black"; // placeholder for now
 }
 
 function clearGrid() {
@@ -19,17 +19,16 @@ function sizeGrid(sideLength) {
 function fillGrid(sideLength) {
   for (let i = 0; i < (sideLength ** 2); i++) {
     const pixel = document.createElement("div");
-    pixel.style.backgroundColor = "rgba(0, 0, 0, 0)"; // format needed for darken mode
-    pixel.addEventListener("mouseover", colorIt);
+    pixel.style.backgroundColor = "white"; // needed for
+    pixel.style.filter = "brightness(1)";  // darken mode
+    pixel.addEventListener("mouseover", darken); // ***here for testing***
     container.appendChild(pixel);
   }
 }
 
 function makeGrid(sideLength) {
   clearGrid();
-
   sizeGrid(sideLength);
-
   fillGrid(sideLength);
 }
 
@@ -46,9 +45,19 @@ makeGrid(sizeSlider.value);
 
 document.getElementById("reset-btn").onclick = function(){makeGrid(sizeSlider.value)};
 
-function randomColor() {
+function colorRandom() {
   let red = Math.floor(Math.random()*255);
   let green = Math.floor(Math.random()*255);
   let blue = Math.floor(Math.random()*255);
-  return `rgba(${red}, ${green}, ${blue}, 1)`;
+  return `rgb(${red}, ${green}, ${blue})`;
+}
+
+function darken(e) {
+  let pixelBrightness = e.target.style.filter.match(/\d(\.\d)?/);
+  
+  if (pixelBrightness === "0") {
+    return;
+  } else {
+    e.target.style.filter = `brightness(${(pixelBrightness[0] - 0.1).toFixed(1)})`;
+  }
 }
